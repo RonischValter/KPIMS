cc9407630105d54db7013f016980bf72324aea35
vronisch <vronisch@NB-VRONISCH>
2023-02-27 13:18:48 +0100

    Alfa 2

---
+++ KPIMS/DeviceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPI_measuring_software
+{
+    internal class DeviceInfo
+    {
+        public string description { get;}
+        public string value { get;}
+        public DeviceInfo(string description, string value) 
+        {
+            this.description = description;
+            this.value = value;
+        }
+    }
+}
--- KPIMS/Program.cs
+++ KPIMS/Program.cs
@@ -11,7 +11,7 @@ namespace KPI_measuring_software
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new main_window());
+            Application.Run(new Main_Window());
         }
     }
 }
